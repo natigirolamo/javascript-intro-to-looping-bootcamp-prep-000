e716cc7ad6dcf20486e321ac3a2d9390d1b90cf0
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-13 00:49:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,17 +1,3 @@
-function forLoop(array){
-  for(var i = 0; i <25; i++){
-    if (i === 1){
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
-  }
-}
-return array
-}
-
-var sample_arr = [];
-console.log(forLoop(sample_arr));
-
 
 function whileLoop(n){
   while(n>0)
