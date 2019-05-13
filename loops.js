9470f8f4b610f7f18ce8677473684665201924c3
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-13 00:59:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,5 @@
-function forLoop(array) {
-   for ( let i = 0; i < 25; i++) {
-     if (i === 1) {
-       array.push(`I am 1 strange loop.`)
-     } else {
-       array.push(`I am ${i} strange loops.`)
-     }
-  }
-  return array;
-}
-
-function whileLoop(n) {
-  let countdown = n;
-
-    while (countdown > 0) {
-      console.log(--countdown)
-    }
-    return "done";
-  }
-
-function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
-  do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
-}
+describe('doWhileLoop(array)', () => {
+    it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
+    it('removes elements from `array` until `array` is empty or until `incrementVariable()` returns `false`', () => {
+      const [array, t] = makeArray()
+      const l = array.length
